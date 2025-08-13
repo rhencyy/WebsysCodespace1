6ebb7a3ffbf39e39e29e92ad016079fd3a6aa86a
rhencyy <rhencydeleon@gmail.com>
2025-08-13 02:14:39 +0000

    main php syntax

---
+++ main.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$name = "Rhency Manansala";
+$age = "18";
+$school = "GRC";
+$course = "BSIT";
+$subject = "WEBSYS";
+$student = "true";
+
+echo "Name:" . $name . "<br>";
+echo "Age:" . $age . "<br>";
+echo "School:" . $school ."<br>";
+echo "Course:" . $course."<br>";
+echo "Subject:" . $subject."<br>";
+?>
+
+</body>
+</html>
